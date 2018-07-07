b5ec3a238ec0a47e1941f78aa73f9a4095162b2f
Unknown <keri.southwood@gmail.com>
2018-07-07 02:38:37 -0400

    Initial version - FizzBuzz to 100
    Prints the numbers from 1 to 100, replacing multiples of 3 with 'Fizz', multiples of 5 with 'Buzz', and multiples of 3 AND 5 with 'FizzBuzz'.

---
+++ FizzBuzz/main.c
@@ -0,0 +1,65 @@
+/***********************************************************************************
+ *  MIT License                                                                    *
+ *                                                                                 *
+ *  Copyright (c) 2018 Keri Southwood-Smith                                        *
+ *                                                                                 *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy   *
+ *  of this software and associated documentation files (the "Software"), to deal  *
+ *  in the Software without restriction, including without limitation the rights   *
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell      *
+ *  copies of the Software, and to permit persons to whom the Software is          *
+ *  furnished to do so, subject to the following conditions:                       *
+ *                                                                                 *
+ *  The above copyright notice and this permission notice shall be included in all *
+ *  copies or substantial portions of the Software.                                *
+ *                                                                                 *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR     *
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,       *
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE    *
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER         *
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,  *
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE  *
+ *  SOFTWARE.                                                                      *
+ ***********************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ *  Classic FizzBuzz problem wherein we print a sequence of numbers, replacing multiples of three
+ *  with "Fizz", multiples of five with "Buzz" and multiples of three AND five with "FizzBuzz."
+ */
+
+int main()
+{
+    for (int i = 1; i <= 100; i++)
+    {
+        if (i % 15 == 0)
+        {
+            printf("FizzBuzz");
+        }
+        else if (i % 3 == 0)
+        {
+            printf("Fizz");
+        }
+        else if (i % 5 == 0)
+        {
+            printf("Buzz");
+        }
+        else
+        {
+            printf("%d", i);
+        }
+
+        if (i % 10 != 0)
+        {
+            printf(", ");
+        }
+        else
+        {
+            printf("\n");
+        }
+    }
+
+    return 0;
+}
